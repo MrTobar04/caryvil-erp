1d744c876faca610ca39ce2117ad1ecea966e96d
Alejandro Javier Hernández Orellana <pedrada914@gmail.com>
2026-09-15 15:43:38 -0600

    revert(theme): Revertir cambios visuales de tema y mantener unicidad funcional de Odoo

--- custom_addons/caryvil_erp/__manifest__.py
+++ custom_addons/caryvil_erp/__manifest__.py
@@ -36,12 +36,8 @@
         'demo/demo_data.xml',
     ],
     'assets': {
-        'web.assets_primary_variables': [
-            'caryvil_erp/static/src/scss/primary_variables.scss',
-        ],
         'web.assets_backend': [
             'caryvil_erp/static/src/scss/custom_theme.scss',
-            'caryvil_erp/static/src/css/custom_theme.css',
         ],
     },
     'installable': True,
