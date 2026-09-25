5c92c99c593d165db359b467af16ecb9fa8dc069
Daniel Ramirez <cesardanielguzmanramirez23@gmail.com>
2026-09-25 14:36:01 -0600

    feat(spec-9.1.1): implementación del registro de ventas con código de barrras, principio activo e IVA

--- custom_addons/caryvil_erp/models/__init__.py
+++ custom_addons/caryvil_erp/models/__init__.py
@@ -8,3 +8,4 @@ from . import res_company_caryvil  # noqa: F401
 from . import caryvil_medicine  # noqa: F401
 from . import stock_lot_medicine  # noqa: F401
 from . import purchase_order_abono  # noqa: F401
+from . import sale_order_medicine  # noqa: F401
